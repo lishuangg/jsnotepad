fd548c7d07110850d0bab79abcaf0188507af4ef
lishuangg <1241982864@qq.com>
2020-04-09 19:27:10 +0800

    add

--- menu-bar/menu-bar.js
+++ menu-bar/menu-bar.js
@@ -1,104 +1,126 @@
-function Menu(){
-    var html =''
-                +'<div class="menu-box">'
-                +'    <ul class="menu-name">'
-                +'    </ul>'
-                +'</div>',
-            cfg;
-    var index = -1;
-    var active = false;
-    var active_num = 0;
-    this.show = function show(conf){
-        cfg = {
-            container:'body',
-        };
-        $.extend(cfg,conf);
-        var $dlg = $(html);
-        $(cfg.container).append($dlg);
-        adddata();
+var $menubar = (function(){
+    var $bar = $('<div class="notepad-menubar"></div>'); 
+    var menuData;//所有菜单数据
+    var menuBox;//容器
+    var id;
+    var num = 1;
+
+    function show(data,box){
+        menuData = data;
+        menuBox = box;
+        console.log(menuData);
+        init();
     }
-    function adddata(){
-        var len = menuData.length;
-        for(var i=0;i<len;i++){
-            var $liT = $('<li class="menu-title"></li>');
-            $('.menu-name').append($liT.html(menuData[i].title));
-            var $ul = $('<ul class="menu-context"></ul>');
-            $ul.css('left',70*i+'px')
-            $('.menu-box').append($ul);
-            (function(i){
-                for(var a=0;a<menuData[i].menuItems.length;a++){
-                    if(menuData[i].menuItems[a].title =='hr'){
-                        var $hr = $('<hr/>')
-                        $ul.append($hr)
-                    }else{
-                        var $liC = $('<li class="menu-item"></li>');
-                        var $sp1 = $('<span class="menu-item-left"></span>')
-                        var $sp2 = $('<span class="menu-item-right"></span>')
-                        if(!menuData[i].menuItems[a].enabled){//区分不能使用和能够使用的按钮
-                            $liC.css('color','#6d6d6d')
-                        }
-                        $ul.append($liC)
-                        $liC.append($sp1.html(menuData[i].menuItems[a].title))
-                        $liC.append($sp2.html(menuData[i].menuItems[a].shortcut));
-                        (function(a){
-                            $liC.click(function(e){
-                                active_num = 0;
-                                active = false;
-                                index = -1;
-                                $('.menu-context').css('display','none')
-                                $('.menu-title').css('background','white')
-                                menuData[i].menuItems[a].handler();
-                            })
-                            $liC.mouseover((e)=>{showSpan(e)})
-                        }(a))
-                    }
+    //构建菜单栏DOM结构
+    function init() {
+        $(menuBox).append($bar);
+        //创建菜单栏
+        var $titles = $('<ul class="menu-title"></ul>');
+        $bar.append($titles);
+        console.log(menuData.length)
+
+        for(var i=0; i<menuData.length; i++) {
+            //创建菜单栏
+            var $title = $('<li class="title">'+menuData[i].title+'</li>'); 
+            $title.attr('data-id', i);
+            $titles.append($title);
+            //创建二级菜单
+            var $menus = $('<ul class="menus"></ul>');
+            $title.append($menus);
+            $menus.css({
+                width: menuData[i].width,
+                left: menuData[i].left,
+                display: 'none'
+            });
+
+            var items = menuData[i].menuItems;
+            for(var j=0; j<items.length; j++) {
+                if(items[j].title === 'hr') {
+                  var $hr = $('<li class="menu-hr"></li>');
+                  $menus.append($hr);
+                  continue;
                 }
-            }(i));
-            (function(i){
-                $liT.click(()=>{titleClick(i)})
-                // $ul.mouseleave(()=>{titleout(i)})
-                $ul.mouseover(()=>{ulOver(i)})
-                $liT.mouseover(()=>{titleStyle(i)})
-                $liT.mouseout(()=>{
-                    $('.menu-title').css('background','white')
-                })
-            }(i))
+                //二级菜单
+                var $menu = $('<li class="menu-item">'+items[j].title+'</li>');
+                $menu.attr('data-x', i);
+                $menu.attr('data-y', j);
+                //右侧快捷键
+                if(items[j].shortcut !== '') {
+                    var $shorcut = $('<span class="shortcut">'+items[j].shortcut+'</span>');
+                    $menu.append($shorcut);
+                } 
+                //按钮禁用         
+                if(!items[j].enabled) $menu.addClass('disabled');
+                //添加二级菜单标题
+                $menus.append($menu);
+            }
+        }
+        titleClick();
+        menuClick();
+    }
+    //点击菜单，显示二级菜单
+    function titleClick(){
+        $(".title").click(function(e){
+            if(num===1){
+                $(this).children().css({ display: 'inline-block' });
+                var i = Number(this.dataset.id);
+                id = i;
+                num++;
+            }
+            else{
+                var i = Number(this.dataset.id);
+                if(id == i){
+                    $(this).children().css({ display: 'none' });
+                    num--;
+                }
+                else{
+                    $(this).children().css({ display: 'inline-block' });  
+                    id = i;
+                }    
+            }
+            // $(this).children().css({ display: 'inline-block' });
+            $(this).siblings().children().css({ display: 'none' });
+            e.stopPropagation();
+        });
+    }
+    //点击二级菜单中的功能
+    function menuClick(){
+        $('.menu-item').click(function(e) {
+            e.stopPropagation();
+            if($(this).hasClass('disabled')) return;
+            var i = this.dataset.x, j = this.dataset.y;
+            $(this).parent().css({ display: 'none' });
+            num--;
+            menuData[i].menuItems[j].handler();
+        });
+    }
+    //设置菜单项是否为勾选状态
+    //isEnabled true 为勾选，false 为取消勾选
+    function checked(row, col, isChecked) {
+        var menuItem = $('.title').eq(row).children('ul').find('.menu-item')[col];
+
+        if(isChecked) {
+            $(menuItem).prepend($('<span class="checked">✓</span>')[0]);
+        } else {
+            $(menuItem).find('.checked').remove();
         }
     }
-    function titleStyle(i){//滑过菜单
-        index = i;
-        $('.menu-title').css('background','white')
-        $('.menu-title').eq(i).css('background','#87CEFA')
-        if(active){
-            $('.menu-context').css('display','none')
-            $('.menu-context').eq(i).css('display','block')
+
+    //设置菜单项为启用或禁用状态
+    //isEnabled true 为启用，false 为禁用
+    function enabled(row, col, isEnabled) {
+        var menuItem = $('.title').eq(row).children('ul').find('.menu-item')[col];
+
+        if(isEnabled) {
+            $(menuItem).removeClass('disabled');
+        } else {
+            $(menuItem).addClass('disabled');
         }
     }
-    function titleClick(i){//点击每项
-        active_num++;
-        if(active_num!=2){
-            active = true;
-            index = i;
-            $('.menu-context').eq(i).css('display','block')
-            $('.menu-title').eq(i).css('background','#87CEFA')
-        }else{//点了俩下关闭子菜单
-            active_num = 0;
-            active = false;
-            index = -1;
-            $('.menu-context').css('display','none')
-        }
-    }
-    function ulOver(i){//划过子菜单
-        $('.menu-title').eq(i).css('background','#87CEFA')
-    }
-    function titleout(i){//划出子菜单
-        $('.menu-title').css('background','white')
-        $('.menu-context').eq(i).css('display','none')
-    }
-    function showSpan(e){//显示子菜单滑动背景变化
-        $('.menu-item').css('background','white')
-        var a = e.target.parentNode
-        console.log(a)
-        $(a).css({background:'#87CEFA'})
-    }
-}
+    
+    return({
+        show:show,
+        checked:checked,
+        enabled:enabled
+    })
+})()
